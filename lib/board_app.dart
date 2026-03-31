19f648ce6846f5e441ab821aa846b6612f73194d
Sander in 't Hout <sander@thenextapp.nl>
2026-04-01 00:26:22 +0200

    Switch font to Ubuntu for consistent look across OS'es

--- lib/board_app.dart
+++ lib/board_app.dart
@@ -1,18 +1,30 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:h3xboard/app_router.dart';
 
 class BoardApp extends StatefulWidget {
+
   const BoardApp({super.key});
 
   @override
   State<BoardApp> createState() => _BoardAppState();
+
 }
 
 class _BoardAppState extends State<BoardApp> {
+
   final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return FluentApp.router(routerConfig: _appRouter.config());
+    return FluentApp.router(
+      routerConfig: _appRouter.config(),
+      theme: FluentThemeData(
+        typography: Typography.fromBrightness(
+          brightness: Brightness.light,
+        ).apply(fontFamily: GoogleFonts.ubuntu().fontFamily),
+      ),
+    );
   }
+
 }
