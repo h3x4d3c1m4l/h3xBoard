cc1a051c8dd9dbedb5a2b5abbe094b8a054b691c
Sander in 't Hout <sander@inthout.me>
2026-05-05 23:37:57 +0200

    Consolidate drawing tool state into DrawingTools model

--- lib/views/board_screen/board_screen_view_model.dart
+++ lib/views/board_screen/board_screen_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:h3xboard/models/board.dart';
+import 'package:h3xboard/models/drawing_tools.dart';
 import 'package:h3xboard/views/base/screen_view_model_base.dart';
-import 'package:h3xboard/views/board_screen/components/toolbars/tool_toolbar.dart';
 import 'package:mobx/mobx.dart';
 
 part 'board_screen_view_model.g.dart';
@@ -12,6 +12,7 @@ abstract class BoardScreenViewModelBase extends ScreenViewModelBase with Store {
 
   @readonly
   Board _board = Board(
+    title: 'Board of ${DateTime.now()}',
     backgroundColor: Colors.white,
     isChalkboard: false,
     linePattern: BoardLinePattern.none,
@@ -20,19 +21,13 @@ abstract class BoardScreenViewModelBase extends ScreenViewModelBase with Store {
   );
 
   @readonly
-  Color? _activeDrawingColor = Colors.black;
-
-  @readonly
-  Color _lastActiveDrawingColor = Colors.black;
-
-  @readonly
-  SelectableEditTool _activeTool = .pen;
-
-  @readonly
-  double _penWidth = 2;
-
-  @readonly
-  double _eraserWidth = 8;
+  DrawingTools _drawingTools = DrawingTools(
+    activeColor: Colors.black,
+    lastActiveColor: Colors.black,
+    activeTool: .pen,
+    penWidth: 2,
+    eraserWidth: 8,
+  );
 
   @readonly
   double _boardPixelRatio = 1;
@@ -43,18 +38,26 @@ abstract class BoardScreenViewModelBase extends ScreenViewModelBase with Store {
 
   @action
   void setActiveColor(Color? color) {
-    _activeDrawingColor = color;
-    if (color != null) _lastActiveDrawingColor = color;
+    _drawingTools = _drawingTools.copyWith(
+      activeColor: color,
+      lastActiveColor: color ?? _drawingTools.lastActiveColor,
+    );
   }
 
   @action
-  void setActiveTool(SelectableEditTool tool) => _activeTool = tool;
+  void setActiveTool(SelectableEditTool tool) {
+    _drawingTools = _drawingTools.copyWith(activeTool: tool);
+  }
 
   @action
-  void setPenWidth(double width) => _penWidth = width;
+  void setPenWidth(double width) {
+    _drawingTools = _drawingTools.copyWith(penWidth: width);
+  }
 
   @action
-  void setEraserWidth(double width) => _eraserWidth = width;
+  void setEraserWidth(double width) {
+    _drawingTools = _drawingTools.copyWith(eraserWidth: width);
+  }
 
   @action
   void updateResizeFactor(BoxConstraints constaints) {
